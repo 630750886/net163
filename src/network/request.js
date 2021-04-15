adf7a3b2f2723afaf484646a155c48eed99f7cb8
630750886 <82512650+630750886@users.noreply.github.com>
2021-04-15 22:48:57 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 export  function request(config) {
     //创建实例
     const instance = axios.create({
-      baseURL:'http://localhost:8888',
+      baseURL:'http://34.92.92.224:8888/',
       timeout:5000
     })
     //发送请求
